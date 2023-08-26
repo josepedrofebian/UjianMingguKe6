d05e5b2e3a211e4b8deb9751e46f0c786113b48e
josepedrofebian <josepedro190299@gmail.com>
2023-08-26 10:51:09 +0700

    second commit

--- src/main/java/com/juaracoding/Soal1.java
+++ src/main/java/com/juaracoding/Soal1.java
@@ -2,15 +2,15 @@ package com.juaracoding;
 
 public class Soal1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("BizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Bizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+        for (int i = 1; i <= 100; i++) { // membuat perulangan dari 1 sampai 100
+            if (i % 3 == 0 && i % 5 == 0) { // mengecek kondisi apakah i merupakan kelipatan 3 dan 5
+                System.out.println("BizzBuzz"); // mencetak BizzBuzz jika kondisi bernilai true
+            } else if (i % 3 == 0) { // mengecek kondisi apakah i merupakan kelipatan 3
+                System.out.println("Bizz"); // mencetak Bizz jika kondisi bernilai true
+            } else if (i % 5 == 0) { // mengecek kondisi apakah i merupakan kelipatan 5
+                System.out.println("Buzz"); // mencetak Buzz jika kondisi bernilai true
+            } else { // mengecek kondisi apakah i bukan kelipatan 3 dan 5, kelipatan 3, dan kelipatan 5
+                System.out.println(i); // mencetak i jika kondisi bernilai true
             }
         }
     }
--- src/main/java/com/juaracoding/Soal2.java
+++ src/main/java/com/juaracoding/Soal2.java
@@ -1,19 +1,50 @@
 package com.juaracoding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Soal2 {
     public static void main(String[] args) {
-        int t = 1;
-        for (int i = 10; i >= 1; i--) {
-            if (i % 2 == 1) {
-                for (int j = 1; j <= i; j++) {
-                    if (j % 2 == 1) {
-                        System.out.print(j);
-                    } else if (j % 2 == 0) {
-                        System.out.print("_");
-                    }
-                }
-                System.out.println();
-            }
+        int n = 9;
+        String pattern = generatePattern(n); // inisialisasi pola awal
+
+        printPattern(pattern); // mencetak pola awal
+
+        for (int i = 0; i < n / 2; i++) {
+            pattern = removeFirstAndLastCharacter(pattern); // menghapus karakter pertama dan terakhir
+            printPattern(pattern); // mencetak pola yang telah diubah
         }
     }
+
+    // method untuk inisiasi pola dengan menambahkan angka dan garis bawah
+    private static String generatePattern(int n) {
+        StringBuilder pattern = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            if (isOdd(i)) {
+                pattern.append(i);
+            } else {
+                pattern.append("_");
+            }
+        }
+
+        return pattern.toString();
+    }
+    // method untuk mengecek apakah number berupa angka ganjil
+    public static boolean isOdd(int number){
+        if(number % 2 == 1){
+            return true;
+        }
+        return false;
+    }
+    // method untuk mencetak pola
+    private static void printPattern(String pattern) {
+        System.out.println(pattern);
+    }
+
+    // method menghapus karakter pertama dan terakhir dari sebuah string
+    public static String removeFirstAndLastCharacter(String str) {
+        return str.substring(1, str.length() - 1);
+    }
 }
+
